738d2d076c9663f71854abe4fe49ff16f8dd6010
lorenzocontic <lorenzocontic@proton.me>
2026-07-15 19:24:13 +0300

    Create scanner.rs

---
+++ scanner.rs
@@ -0,0 +1,15 @@
+use crate::models::Screenshot;
+
+pub struct Scanner;
+
+impl Scanner {
+
+    pub fn scan(
+        files: Vec<Screenshot>
+    ) -> Vec<Screenshot> {
+
+        files
+
+    }
+
+}
